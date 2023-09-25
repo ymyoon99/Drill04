be79c36463dd77c2787b1bd8508e2e1d881c5745
ymyoon(OMEN) <ozone753@naver.com>
2023-09-26 04:29:16 +0900

    캐릭터 상하좌우 이동구현, idle 상태와 run 상태 애니메이션 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,7 +8,7 @@ run = load_image('char_run.png')
 idle = load_image('char_idle.png')
 
 def handle_events():
-    global running, x, y, dir
+    global running, x, y, dir, dir2
 
     events = get_events()
     for event in events:
@@ -17,28 +17,53 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
+                is_idle = False
             elif event.key == SDLK_LEFT:
                 dir -= 1
+                is_idle = False
+            elif event.key == SDLK_UP:
+                dir2 += 1
+                is_idle = False
+            elif event.key == SDLK_DOWN:
+                dir2 -= 1
+                is_idle = False
             elif event.key == SDLK_ESCAPE:
-                running == False
+                running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir -= 1
             elif event.key == SDLK_LEFT:
                 dir += 1
+            elif event.key == SDLK_UP:
+                dir2 -= 1
+            elif event.key == SDLK_DOWN:
+                dir2 += 1
+        elif event.key in (SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN):
+                dir = 0
+                is_idle = True
 
 running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+is_idle = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2 # 맵 중앙에 생성
 frame = 0
 dir = 0 # dir이 0일때 정지
+dir2 = 0
 
 while running:
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    run.clip_draw(frame * 120, 0, 100, 100, x, 200, 200, 200)
+
+    if dir != 0 or dir2 != 0:  # 이동 중인 경우
+        is_idle = False
+        run.clip_draw(frame * 120, 0, 100, 100, x, y, 200, 200)
+    else:  # 정지 상태인 경우
+        is_idle = True
+        idle.clip_draw(frame * 120, 0, 100, 100, x, y, 200, 200)
+
     update_canvas()
     handle_events()
     frame = (frame + 1) % 10
     x += dir * 5
+    y += dir2 * 5
     delay(0.05)
 
 close_canvas()
