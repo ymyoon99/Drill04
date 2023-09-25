4917c25ff682d2a6b28f5ea4e916c5f76551a246
ymyoon(OMEN) <ozone753@naver.com>
2023-09-26 03:53:50 +0900

    캐릭터 cliping과 좌우이동 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,5 +8,37 @@ run = load_image('char_run.png')
 idle = load_image('char_idle.png')
 
 def handle_events():
+    global running, x, y, dir
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running == False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+dir = 0 # dir이 0일때 정지
+
+while running:
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    run.clip_draw(frame * 120, 0, 100, 100, x, 200, 200, 200)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 10
+    x += dir * 5
+    delay(0.05)
 
 close_canvas()
